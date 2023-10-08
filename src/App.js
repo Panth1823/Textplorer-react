d8b4b22a0883b7b94e2e7f07a895c8e2925c4d02
Irfan <irfansalim77@gmail.com>
2023-10-08 21:30:01 +0530

    Updated the UI to align divs in the center and some other changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import "./App.css";
 import Form from "./components/Form";
 import Navbar from "./components/Navbar";
 
-
 function App() {
-  return (
-    <>
-    <div className="body">
-      <Navbar />
-      <div className="container">
-        <Form />
-      
-      
-      </div>
-      </div> 
-   
-    </>
-  );
+    return (
+        <>
+            <div className="body">
+                <Navbar />
+                <div className="container flex flex-col justify-center items-center m-auto">
+                    <Form />
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
